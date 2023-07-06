66407ac321ce167e1f580193ca21836fad6a17b3
Jeremy <87815518+jdjaxon@users.noreply.github.com>
2023-07-06 11:58:28 -0400

    Updating treesitter.

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -18,7 +18,7 @@ require'nvim-treesitter.configs'.setup {
         disable = { "" },
         additional_vim_regex_highlighting = true,
     },
-    indent = { enable = true, disable = { "yaml" } },
+    indent = { enable = false, disable = { "yaml" } },
     context_commentstring = {
         enable = true,
         enable_autocmd = false,
