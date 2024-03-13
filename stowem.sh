f9d72c74c02728f6e32d5d6988b3ca1e5626c963
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2024-03-13 16:11:05 -0400

    Improved stowem.sh script to improve directory parsing and error handling.

--- stowem.sh
+++ stowem.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-for app in $(ls | grep -v *.sh)
-do
-    echo "Stowing $app..."
-    stow "$app"
+# Navigate to the script's directory or exit if it fails.
+cd "$(dirname "$0")" || exit 1
+
+# Iterate over all directories in the current location.
+shopt -s nullglob
+for app in */; do
+    echo "Stowing ${app%/}..."
+    if ! stow "${app%/}"; then
+        echo "Error stowing ${app%/}"
+        exit 1
+    fi
 done
