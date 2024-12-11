79d77e3e54d183283014e1e4b2432a724215a624
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2024-12-11 14:49:51 -0500

    Added functionality for pyright to dynamically detect the local virtual environment for python projects.

--- nvim/.config/nvim/lua/user/lsp/settings/pyright.lua
+++ nvim/.config/nvim/lua/user/lsp/settings/pyright.lua
@@ -1,8 +1,17 @@
+local function get_python_site_packages()
+    local python_path = vim.g.python3_host_prog or vim.fn.exepath("python3")
+    local site_packages = vim.fn.systemlist(python_path .. " -c 'import site; print(site.getsitepackages()[0])'")[1]
+    return site_packages
+end
+
 return {
   settings = {
     python = {
       analysis = {
+        autoSearchPaths = true,
+        usLibraryCodeForTypes = true,
         typeCheckingMode = "off",
+        extraPaths = { get_python_site_packages() }
       },
     },
   },
