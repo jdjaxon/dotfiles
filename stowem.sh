e52e7124725dc1deb8e6e4275db63d88c508ad19
Jeremy Jackson <87815518+jdjaxon@users.noreply.github.com>
2025-04-04 21:09:09 -0400

    Add dry-run functionality to stowem script.

--- stowem.sh
+++ stowem.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
-# Navigate to the script's directory or exit if it fails.
 cd "$(dirname "$0")" || exit 1
 
-# Iterate over all directories in the current location.
+DRY_RUN=false
+if [[ "$1" == "--dry-run" ]]; then
+    DRY_RUN=true
+    echo "[Dry Run Mode Enabled]"
+fi
+
 shopt -s nullglob
 for app in */; do
-    echo "Stowing ${app%/}..."
-    if ! stow "${app%/}"; then
-        echo "Error stowing ${app%/}"
-        exit 1
+    app_name="${app%/}"
+    echo "Stowing ${app_name}..."
+
+    if $DRY_RUN; then
+        stow --no --verbose=2 "${app_name}"
+        echo
+    else
+        if ! stow "${app_name}"; then
+            echo "Error stowing ${app_name}"
+            exit 1
+        fi
     fi
 done
